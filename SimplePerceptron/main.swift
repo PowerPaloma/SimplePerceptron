7d7ee54bfa511a71aa7a5aea3915aeef8bcdc712
Paloma Bispo <palomabispo@alu.ufc.br>
2018-09-11 13:31:34 -0300

    Initial Commit

---
+++ SimplePerceptron/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimplePerceptron
+//
+//  Created by Ada 2018 on 11/09/2018.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
